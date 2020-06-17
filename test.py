72bedcf14c97bb61757b16cfc380cc60dbf649f7
Khaled Nasr <khaled.n.leila@gmail.com>
2020-06-17 20:25:22 +0200

    modify the models with association object

--- test.py
+++ test.py
@@ -9,6 +9,8 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+from sqlalchemy.ext.associationproxy import association_proxy
+
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -16,6 +18,7 @@ from forms import *
 app = Flask(__name__)
 moment = Moment(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:passwort@localhost:5432/testy'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
@@ -25,24 +28,31 @@ migrate = Migrate(app, db)
 # Models.
 #----------------------------------------------------------------------------#
 
-Show = db.Table('Show',
-    db.Column('Venue_id', db.Integer, db.ForeignKey('Venue.id'), primary_key = True),
-    db.Column('Artist.id', db.Integer, db.ForeignKey('Artist.id'), primary_key = True)
-)
+class Show(db.Model):
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), primary_key = True)
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), primary_key = True)
+    start_time = db.Column(db.DateTime, nullable = False)
+    artist = db.relationship('Artist', backref = db.backref('shows', cascade = "all, delete-orphan"))
+    venue = db.relationship('Venue', backref = db.backref('shows', cascade = "all, delete-orphan"))
+
+
 
 class Venue(db.Model):
     __tablename__ = 'Venue'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable = False)
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-    start_time = db.Column(db.DateTime, nullable = False)
-    artists = db.relationship('Artist', secondary=Show, backref=db.backref('venues', lazy = True))
+    website = db.Column(db.String(120))
+    seeking_description = db.Column(db.String)
+    seeking_talent = db.Column(db.Boolean, default = True)
+
+    artists = association_proxy('shows', 'artist' , cascade_scalar_deletes= True)
 
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
@@ -51,13 +61,18 @@ class Artist(db.Model):
     __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable = False)
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    seeking_description = db.Column(db.String)
+    seeking_venue = db.Column(db.Boolean, default = True)
+    website = db.Column(db.String(120))
+
+    venues = association_proxy('shows', 'venue' , cascade_scalar_deletes= True)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
@@ -65,7 +80,7 @@ class Artist(db.Model):
 
 
 
-db.drop_all()
+# db.drop_all()
 db.create_all()
 
 
